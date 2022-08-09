c48e8630186bc3074a5df38dbde51b7401906c88
Redsparrow77 <jayyzero77@gmail.com>
2022-08-09 15:37:42 +0700

    feat: unit testing Implementing unit testing using Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	testEnvironment: 'jsdom',
+	testPathIgnorePatterns: ['/node_modules/'],
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.js(x)'],
+	// setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+	modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+}
