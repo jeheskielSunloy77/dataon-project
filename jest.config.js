b485f7a47921bb0423722382ad5f9b19c24027ab
Redsparrow77 <jayyzero77@gmail.com>
2022-08-12 18:56:35 +0700

    fix: Removing unused jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	testEnvironment: 'jsdom',
-	testPathIgnorePatterns: ['/node_modules/'],
-	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.js(x)'],
-	modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
-}
