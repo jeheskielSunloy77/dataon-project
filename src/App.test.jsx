655c2ccf0b3e4d92c40c1f91693ef40756469035
Redsparrow77 <jayyzero77@gmail.com>
2022-08-15 11:50:31 +0700

    fix: Fixed App.test.jsx unit test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
 import { render } from '@testing-library/react'
 // import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 test('renders without crashing', () => {
-	render(<App />)
+	render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	)
 })
