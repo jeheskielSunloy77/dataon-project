98c4a8a98cf8f00e96d17aea41f5d66a7afca00a
Redsparrow77 <jayyzero77@gmail.com>
2022-08-16 21:03:26 +0700

    fix: Moving Table to a new file component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 import Breadcrumbs from './Breadcrumbs'
 import Searchbar from './Searchbar'
+import TableData from './TableData'
 
-export { Breadcrumbs, Searchbar }
+export { Breadcrumbs, Searchbar, TableData }
