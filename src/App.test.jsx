d2c23f852c15fe99d650f5a696a2f0a81ff8dd44
Redsparrow77 <jayyzero77@gmail.com>
2022-08-23 10:10:17 +0700

    fix: fixing unit test at App.test.jsx and index.css adding style

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,15 @@
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AppProvider } from './utils/AppContext'
 import { matchMediaConfig } from './utils/testUtils'
 matchMediaConfig()
 test('renders without crashing', () => {
 	render(
 		<BrowserRouter>
-			<App />
+			<AppProvider>
+				<App />
+			</AppProvider>
 		</BrowserRouter>
 	)
 })
