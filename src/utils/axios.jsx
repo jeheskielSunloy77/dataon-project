7d2bc76e4befb67c67bdb6db2991854543b2aebd
Redsparrow77 <jayyzero77@gmail.com>
2022-08-24 12:33:54 +0700

    feat: Setting up axios with new API 1. adding new baseURL on axios instance 2. testing if the API endpoint is available

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,15 @@
 import axios from 'axios'
 
+const baseURL = 'https://6304641b761a3bce77e6d7fd.mockapi.io/api/v3/'
+
+fetch(baseURL + 'trainings').then((response) => {
+	if (!response.ok) {
+		throw new Error('HTTP status ' + response.status)
+	} else {
+		return baseURL
+	}
+})
+
 export default axios.create({
-	baseURL: 'https://randomuser.me/api/',
+	baseURL,
 })
