a8f2b95aed4298db6d0d26a2a3cbd74bd910f198
Redsparrow77 <jayyzero77@gmail.com>
2022-08-29 13:21:43 +0700

    feat: Adding new page DetailTraining page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
-import DetailArticle from './DetailArticle'
+import DetailTraining from './DetailTraining'
 import EditArticle from './EditArticle'
 import Home from './Home'
 import Login from './Login'
 import NewTraining from './NewTraining'
 
-export { Home, Login, DetailArticle, EditArticle, NewTraining }
+export { Home, Login, DetailTraining, EditArticle, NewTraining }
