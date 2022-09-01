cabd7114be51fbd328869c1020ad7ee1646a3d41
Redsparrow77 <jayyzero77@gmail.com>
2022-09-01 16:36:39 +0700

    feat: refactoring

--- src/components/TrainingTable/index.jsx
+++ src/components/TrainingTable/index.jsx
@@ -3,85 +3,87 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 import { Link } from 'react-router-dom'
 import { RatingScore } from '../index'
 
-const TrainingTable = ({ tableData, setPageLimit, infiniteScroll }) => {
-	const Wrapper = ({ children }) => {
-		const fetchMoreData = () => {
-			if (tableData.length >= 9) {
-				return false
-			} else setPageLimit((prev) => prev + 5)
-		}
+const TrainingTable = ({ tableData, setPageLimit, infiniteScroll }) => (
+	<Wrapper
+		tableData={tableData}
+		setPageLimit={setPageLimit}
+		infiniteScroll={infiniteScroll}
+	>
+		<Table
+			dataSource={tableData}
+			pagination={!infiniteScroll}
+			className='overflow-x-auto rounded-lg'
+		>
+			<Table.Column
+				title='Event Name'
+				dataIndex='name'
+				key='name'
+				render={(text, { id }) => (
+					<Link to={`/detailTraining/${id}`} className='text-blue-500 font-medium'>
+						{text}
+					</Link>
+				)}
+				sorter={(a, b) => a.name.length - b.name.length}
+			/>
+			<Table.Column
+				title='Event Type'
+				dataIndex='isOnline'
+				key='isOnline'
+				render={(isOnline) => (isOnline ? 'Online Class' : 'Offline Class')}
+				sorter={(a, b) => a.isOnline - b.isOnline}
+			/>
+			<Table.Column
+				title='Event Period'
+				dataIndex='period'
+				key='period'
+				sorter={(a, b) => Date.parse(a.startDate) - Date.parse(b.startDate)}
+			/>
+			<Table.Column
+				title='Trainer Name'
+				dataIndex='trainerName'
+				key='trainerName'
+				sorter={(a, b) => a.trainerName.length - b.trainerName.length}
+			/>
+			<Table.Column
+				title='Rating'
+				dataIndex='rating'
+				key='rating'
+				render={(rating) => <RatingScore score={rating} />}
+				sorter={(a, b) => a.rating - b.rating}
+			/>
+			<Table.Column
+				title='Aditional Info'
+				dataIndex='information'
+				className='max-w-xs'
+				key='information'
+				sorter={(a, b) => a.information.length - b.information.length}
+			/>
+		</Table>
+	</Wrapper>
+)
 
-		if (infiniteScroll)
-			return (
-				<InfiniteScroll
-					dataLength={tableData.length}
-					next={fetchMoreData}
-					hasMore={tableData.length < 9}
-					loader={
-						<Spin className='absolute bottom-0 left-1/2 -translate-x-1/2' spinning />
-					}
-					className='relative'
-				>
-					{children}
-				</InfiniteScroll>
-			)
-		else return children
+const Wrapper = ({ children, infiniteScroll, tableData, setPageLimit }) => {
+	const fetchMoreData = () => {
+		if (tableData.length >= 9) {
+			return false
+		} else setPageLimit((prev) => prev + 5)
 	}
 
-	return (
-		<Wrapper>
-			<Table
-				dataSource={tableData}
-				pagination={!infiniteScroll}
-				className='overflow-x-auto rounded-lg'
+	if (infiniteScroll)
+		return (
+			<InfiniteScroll
+				dataLength={tableData.length}
+				next={fetchMoreData}
+				hasMore={tableData.length < 9}
+				loader={
+					<Spin className='absolute bottom-0 left-1/2 -translate-x-1/2' spinning />
+				}
+				className='relative'
 			>
-				<Table.Column
-					title='Event Name'
-					dataIndex='name'
-					key='name'
-					render={(text, { id }) => (
-						<Link to={`/detailTraining/${id}`} className='text-blue-500 font-medium'>
-							{text}
-						</Link>
-					)}
-					sorter={(a, b) => a.name.length - b.name.length}
-				/>
-				<Table.Column
-					title='Event Type'
-					dataIndex='isOnline'
-					key='isOnline'
-					render={(isOnline) => (isOnline ? 'Online Class' : 'Offline Class')}
-					sorter={(a, b) => a.isOnline - b.isOnline}
-				/>
-				<Table.Column
-					title='Event Period'
-					dataIndex='period'
-					key='period'
-					sorter={(a, b) => Date.parse(a.startDate) - Date.parse(b.startDate)}
-				/>
-				<Table.Column
-					title='Trainer Name'
-					dataIndex='trainerName'
-					key='trainerName'
-					sorter={(a, b) => a.trainerName.length - b.trainerName.length}
-				/>
-				<Table.Column
-					title='Rating'
-					dataIndex='rating'
-					key='rating'
-					render={(rating) => <RatingScore score={rating} />}
-					sorter={(a, b) => a.rating - b.rating}
-				/>
-				<Table.Column
-					title='Aditional Info'
-					dataIndex='information'
-					className='max-w-xs'
-					key='information'
-					sorter={(a, b) => a.information.length - b.information.length}
-				/>
-			</Table>
-		</Wrapper>
-	)
+				{children}
+			</InfiniteScroll>
+		)
+	else return children
 }
 
 export default TrainingTable
