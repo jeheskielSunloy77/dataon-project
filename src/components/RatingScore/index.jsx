525793a01719047c31bc5c9dbe4bee8e52471836
Redsparrow77 <jayyzero77@gmail.com>
2022-09-01 10:43:39 +0700

    feat:Create Modal Form Submit Ratings

--- src/components/RatingScore/index.jsx
+++ src/components/RatingScore/index.jsx
@@ -1,11 +1,38 @@
-import { Icon } from '../index'
+import { Modal, Rate } from 'antd'
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
 
 const RatingScore = ({ score }) => {
-	const stars = [...Array(5)].map((_, index) => (
-		<Icon key={index} name='Star' active={index < score} />
-	))
+	const [modal, setModal] = useState(null)
 
-	return stars
+	return (
+		<>
+			<div onClick={() => setModal(true)}>
+				<Rate className='cursor-pointer' disabled defaultValue={score} />
+			</div>
+			<SubmitRatingModal modal={modal} setModal={setModal} score={score} />
+		</>
+	)
+}
+
+const SubmitRatingModal = ({ modal, setModal, score }) => {
+	const submitRating = () => {
+		setModal(false)
+		// TODO: POST rating to the API
+	}
+
+	if (modal)
+		return createPortal(
+			<Modal
+				title='Submit Rating'
+				visible={modal}
+				onOk={submitRating}
+				onCancel={() => setModal(false)}
+			>
+				<Rate className='centerAbsolute' defaultValue={score} />
+			</Modal>,
+			document.getElementById('root')
+		)
 }
 
 export default RatingScore
