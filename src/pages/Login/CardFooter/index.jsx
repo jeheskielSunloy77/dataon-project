7b4dfd57888ee863044c01d02ce5d029a6ed0e52
Redsparrow77 <jayyzero77@gmail.com>
2022-09-14 15:26:03 +0700

    faet: update

--- src/pages/Login/CardFooter/index.jsx
+++
@@ -1,8 +0,0 @@
-const CardFooter = () => (
-	<p className='text-center w-2/3 mx-auto'>
-		This product is licensed for Dataon Corporation <br />© 1999 - 2022 Dataon
-		Technologies, All Rights Reserved
-	</p>
-)
-
-export default CardFooter
--- src/pages/Login/CardHeader/index.jsx
+++ src/pages/Login/CardHeader/index.jsx
@@ -1,24 +1,46 @@
 import mainLogo from '@/assets/mainLogo.png'
 import { Select } from 'antd'
+import { useTranslation } from 'react-i18next'
 
-const CardHeader = () => (
-	<div className='flex gap-2'>
-		<img
-			src={mainLogo}
-			width='100'
-			height='60'
-			alt='main logo'
-			data-testid='mainLogo'
-		/>
-		<span>
-			Human Resource Information System
-			<h1 className='font-bold text-xl'>SunFish7</h1>
-		</span>
-		<Select defaultValue='en' className='ml-auto' data-testid='langSelect'>
-			<Select.Option value='en'>English(EN)</Select.Option>
-			<Select.Option value='id'>Bahasa(ID)</Select.Option>
-		</Select>
-	</div>
-)
+const CardHeader = () => {
+	const { t, i18n } = useTranslation()
+
+	const setLanguage = (value) => {
+		i18n.changeLanguage(value)
+		localStorage.setItem('lng', value)
+	}
+
+	return (
+		<>
+			<div className='flex gap-2'>
+				<img
+					src={mainLogo}
+					width='100'
+					height='60'
+					alt='main logo'
+					data-testid='mainLogo'
+				/>
+				<span>
+					{t('Human Resource Information System')}
+					<h1 className='font-bold text-xl'>SunFish7</h1>
+				</span>
+				<Select
+					defaultValue={localStorage.getItem('lng') || 'en'}
+					className='ml-auto'
+					data-testid='langSelect'
+					onChange={(value) => setLanguage(value)}
+				>
+					<Select.Option value='en'>English(EN)</Select.Option>
+					<Select.Option value='id'>Bahasa(ID)</Select.Option>
+				</Select>
+			</div>
+			<style jsx>{`
+				.ant-select-arrow {
+					top: 17px !important;
+				}
+			`}</style>
+		</>
+	)
+}
 
 export default CardHeader
