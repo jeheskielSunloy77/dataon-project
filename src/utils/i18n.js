91aa17f2b04989bbbc2fa1c6216b63c2cb88d380
Redsparrow77 <jayyzero77@gmail.com>
2022-09-14 15:24:47 +0700

    faet: update

---
+++ src/utils/i18n.js
@@ -0,0 +1,271 @@
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18next.use(initReactI18next).init({
+	resources: {
+		en: {
+			translation: {
+				'Human Resource Information System': 'Human Resource Information System',
+				'Please enter your cridentials to access the system':
+					'Please enter your cridentials to access the system',
+				'Sign In': 'Sign In',
+
+				Password: 'Password',
+				'Enter Your Username': 'Enter Your Username',
+				'Enter Your Password': 'Enter Your Password',
+
+				'Please Enter Your Username': 'Please Enter Your Username',
+				'Please Enter Your Password': 'Please Enter Your Password',
+
+				'Username can only be an alphanumeric value':
+					'Username can only be an alphanumeric value',
+				'Username must be between 5 and 20 characters':
+					'Username must be between 5 and 20 characters',
+
+				'Password must be between 5 and 20 characters':
+					'Password must be between 5 and 20 characters',
+
+				'Forgot Password': 'Forgot Password',
+				'Remember Me': 'Remember Me',
+				'Forgot Password?': 'Forgot Password?',
+
+				'This product is licensed for Dataon Corporation':
+					'This product is licensed for Dataon Corporation',
+				'1999 - 2022 Dataon Technologies, All Rights Reserved':
+					'1999 - 2022 Dataon Technologies, All Rights Reserved',
+
+				Home: 'Home',
+				'Create New Training': 'Create New Training',
+				Logout: 'Logout',
+				More: 'More',
+
+				'Search Training': 'Search Training',
+				'Search Training Name': 'Search Training Name',
+				'Search Event Type': 'Search Event Type',
+				'Search Event Status': 'Search Event Status',
+				'Event Status': 'Event Status',
+				'Related Jobs Only': 'Related Jobs Only',
+				'View As Cards': 'View As Cards',
+				'View As Table': 'View As Table',
+
+				'My Trainings Sessions': 'My Trainings Sessions',
+				'All Trainings Sessions': 'All Trainings Sessions',
+
+				'Event Number': 'Event Number',
+				'Event Name': 'Event Name',
+				'Event Type': 'Event Type',
+				'Trainer Name': 'Trainer Name',
+				'Event Period': 'Event Period',
+				'Online Class': 'Online Class',
+				'Offline Class': 'Offline Class',
+				Rating: 'Rating',
+				'Aditional Info': 'Aditional Info',
+
+				'Thank you for your feedback': 'Thank you for your feedback',
+				'Rating submitted successfully': 'Rating submitted successfully',
+
+				'Your All Cought Up!': 'Your All Cought Up!',
+
+				'Event Completed': 'Event Completed',
+				'Upcoming Event': 'Upcoming Event',
+				'Give Feedback': 'Give Feedback',
+				'View Location': 'View Location',
+
+				'Training saved successfully': 'Training saved successfully',
+				'Are you sure you want to save this training?':
+					'Are you sure you want to save this training?',
+				'Please select the Event Type!': 'Please select the Event Type!',
+				'Training Course': 'Training Course',
+				Location: 'Location',
+				Longitude: 'Longitude',
+				Latitude: 'Latitude',
+				'Provider Type': 'Provider Type',
+				Provider: 'Provider',
+				'Select A Provider': 'Select A Provider',
+				'Provider 1': 'Provider 1',
+				'Provider 2': 'Provider 2',
+				'Event Thumbnail': 'Event Thumbnail',
+				'Click To Upload': 'Click To Upload',
+				'Recomended image resolution is 500x300 (5:3 aspect ratio) with max 2MB .jpeg/jpg':
+					'Recomended image resolution is 500x300 (5:3 aspect ratio) with max 2MB .jpeg/jpg',
+				Date: 'Date',
+				'Please input the date!': 'Please input the date!',
+				'Open For Registration': 'Open For Registration',
+				'Please fill in the aditional information!':
+					'Please fill in the aditional information!',
+				Participants: 'Participants',
+
+				'Are you sure you want to delete this training?':
+					'Are you sure you want to delete this training?',
+				'You are not authorized to delete this training':
+					'You are not authorized to delete this training',
+				'Only admin can delete trainings, please contact admin for more information.':
+					'Only admin can delete trainings, please contact admin for more information.',
+				'You are not authorized to edit this training':
+					'You are not authorized to edit this training',
+				'Only admin can edit trainings, please contact admin for more information.':
+					'Only admin can edit trainings, please contact admin for more information.',
+				Confirm: 'Confirm',
+				'Thank you for joining this training':
+					'Thank you for joining this training',
+				'You can check your training in My Training page, And we will see you there!':
+					'You can check your training in My Training page, And we will see you there!',
+				'Training Class': 'Training Class',
+				'Join Class': 'Join Class',
+				'Joined Team': 'Joined Team',
+				'Invite Others': 'Invite Others',
+				'Please select the event status!': 'Please select the event status!',
+				Overview: 'Overview',
+				Resources: 'Resources',
+				Instructor: 'Instructor',
+				'Professional Trainer': 'Professional Trainer',
+				'Target Participants': 'Target Participants',
+				Students: 'Students',
+				'End Date': 'End Date',
+				Back: 'Back',
+				Delete: 'Delete',
+				Edit: 'Edit',
+
+				'edit-training': 'edit-training',
+				'new-training': 'new-training',
+
+				'Sorry, We couldnt find what you are looking for!':
+					'Sorry, We couldnt find what you are looking for!',
+				'Go Back': 'Go Back',
+			},
+		},
+		id: {
+			translation: {
+				'Human Resource Information System': 'Sistem Informasi Sumber Daya Manusia',
+				'Please enter your cridentials to access the system':
+					'Silahkan masukkan kredensial anda untuk mengakses sistem',
+				'Sign In': 'Masuk',
+
+				Password: 'Kata Sandi',
+				'Enter Your Username': 'Masukkan Username Anda',
+				'Enter Your Password': 'Masukkan Password Anda',
+
+				'Please Enter Your Username': 'Silahkan Masukkan Username Anda',
+				'Please Enter Your Password': 'Silahkan Masukkan Password Anda',
+
+				'Username can only be an alphanumeric value':
+					'Username hanya boleh berupa huruf dan angka',
+				'Username must be between 5 and 20 characters':
+					'Username harus berjumlah 5-20 karakter',
+
+				'Password must be between 5 and 20 characters':
+					'Password harus berjumlah 5-20 karakter',
+
+				'Forgot Password': 'Lupa Password',
+				'Remember Me': 'Ingat Saya',
+				'Forgot Password?': 'Lupa Password?',
+
+				'This product is licensed for Dataon Corporation':
+					'Produk ini dilisensikan untuk Dataon Corporation',
+				'1999 - 2022 Dataon Technologies, All Rights Reserved':
+					'1999 - 2022 Dataon Technologies, Hak Cipta Dilindungi Undang-undang',
+
+				Home: 'Beranda',
+				'Create New Training': 'Buat Pelatihan Baru',
+				More: 'Lainnya',
+				Logout: 'Keluar',
+
+				'Search Training': 'Cari Pelatihan',
+				'Search Training Name': 'Cari Nama Pelatihan',
+				'Search Event Type': 'Cari Jenis Pelatihan',
+				'Search Event Status': 'Cari Status Pelatihan',
+				'Event Status': 'Status Pelatihan',
+				'Related Jobs Only': 'Hanya Pelatihan Terkait',
+				'View As Cards': 'Tampilan Kartu',
+				'View As Table': 'Tampilan Tabel',
+
+				'My Trainings Sessions': 'Sesi Pelatihan Saya',
+				'All Trainings Sessions': 'Semua Sesi Pelatihan',
+
+				'Event Number': 'Nomor Pelatihan',
+				'Event Name': 'Nama Pelatihan',
+				'Event Type': 'Jenis Pelatihan',
+				'Trainer Name': 'Nama Pelatih',
+				'Event Period': 'Periode Pelatihan',
+				'Online Class': 'Kelas Online',
+				'Offline Class': 'Kelas Offline',
+				Rating: 'Penilaian',
+				'Aditional Info': 'Info Tambahan',
+
+				'Thank you for your feedback': 'Terima kasih atas masukan anda',
+				'Rating submitted successfully': 'Rating berhasil dikirim',
+
+				'Your All Cought Up!': 'Anda Sudah Melihat Semuanya!',
+
+				'Event Completed': 'Pelatihan Selesai',
+				'Upcoming Event': 'Pelatihan Akan Datang',
+				'Give Feedback': 'Beri Masukan',
+				'View Location': 'Lihat Lokasi',
+
+				'Training saved successfully': 'Pelatihan berhasil disimpan',
+				'Are you sure you want to save this training?':
+					'Apakah anda yakin ingin menyimpan pelatihan ini?',
+				'Please select the Event Type!': 'Silahkan pilih jenis pelatihan!',
+				'Training Course': 'Kursus Pelatihan',
+				Location: 'Lokasi',
+				Longitude: 'Garis Bujur',
+				Latitude: 'Garis Lintang',
+				'Provider Type': 'Jenis Penyelenggara',
+				Provider: 'Penyelenggara',
+				'Select A Provider': 'Pilih Penyelenggara',
+				'Provider 1': 'Penyelenggara 1',
+				'Provider 2': 'Penyelenggara 2',
+				'Event Thumbnail': 'Thumbnail Pelatihan',
+				'Click To Upload': 'Klik Untuk Mengunggah',
+				'Recomended image resolution is 500x300 (5:3 aspect ratio) with max 2MB .jpeg/jpg':
+					'Resolusi gambar yang direkomendasikan adalah 500x300 (rasio aspek 5:3) dengan maksimal 2MB .jpeg/jpg',
+				Date: 'Tanggal',
+				'Please input the date!': 'Silahkan masukkan tanggal!',
+				'Open For Registration': 'Buka Untuk Pendaftaran',
+				'Please fill in the aditional information!':
+					'Silahkan isi informasi tambahan!',
+				Participants: 'Peserta',
+
+				'Are you sure you want to delete this training?':
+					'Apakah anda yakin ingin menghapus pelatihan ini?',
+				'You are not authorized to delete this training':
+					'Anda tidak memiliki hak untuk menghapus pelatihan ini',
+				'Only admin can delete trainings, please contact admin for more information.':
+					'Hanya admin yang dapat menghapus pelatihan, silahkan hubungi admin untuk informasi lebih lanjut.',
+				'You are not authorized to edit this training':
+					'Anda tidak memiliki hak untuk mengedit pelatihan ini',
+				'Only admin can edit trainings, please contact admin for more information.':
+					'Hanya admin yang dapat mengedit pelatihan, silahkan hubungi admin untuk informasi lebih lanjut.',
+				Confirm: 'Konfirmasi',
+				'Thank you for joining this training': 'Terima kasih telah bergabung',
+				'You can check your training in My Training page, And we will see you there!':
+					'Anda dapat memeriksa pelatihan anda di halaman Pelatihan Saya, dan kami akan melihat anda di sana!',
+				'Training Class': 'Kelas Pelatihan',
+				'Join Class': 'Gabung Kelas',
+				'Joined Team': 'Tim yang Bergabung',
+				'Invite Others': 'Undang Orang Lain',
+				'Please select the event status!': 'Silahkan pilih status pelatihan!',
+				Overview: 'Ringkasan',
+				Resources: 'Sumber Daya',
+				Instructor: 'Instruktur',
+				'Professional Trainer': 'Pelatih Profesional',
+				'Target Participants': 'Target Peserta',
+				Students: 'Mahasiswa',
+				'End Date': 'Tanggal Berakhir',
+				Back: 'Kembali',
+				Delete: 'Hapus',
+				Edit: 'Ubah',
+
+				'edit-training': 'ubah-pelatihan',
+				'new-training': 'pelatihan-baru',
+
+				'Sorry, We couldnt find what you are looking for!':
+					'Maaf, kami tidak dapat menemukan apa yang anda cari!',
+				'Go Back': 'Kembali',
+			},
+		},
+	},
+	lng: localStorage.getItem('lng') || 'en',
+})
+
+export default i18next
---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+import { AppContext, AppProvider } from './AppContext'
+import customAxios from './axios'
+import getUser from './getUser'
+import i18n from './i18n'
+import parsePeriod from './parsePeriod'
+import queryPrams from './queryParams'
+import { matchMediaConfig } from './testUtils'
+
+export {
+	AppContext,
+	AppProvider,
+	parsePeriod,
+	queryPrams,
+	customAxios,
+	matchMediaConfig,
+	i18n,
+	getUser,
+}
