fe1524a7b19faa990e800f2407ea875437336f97
Redsparrow77 <jayyzero77@gmail.com>
2022-09-14 15:26:47 +0700

    faet: update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,6 @@ import DetailTraining from './DetailTraining'
 import Home from './Home'
 import Login from './Login'
 import NewTraining from './NewTraining'
+import Page404 from './Page404'
 
-export { Home, Login, DetailTraining, NewTraining }
+export { Home, Login, DetailTraining, NewTraining, Page404 }
