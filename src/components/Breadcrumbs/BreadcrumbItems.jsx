cd04252f06f8dc6ffa094a740ba9532f1056c81e
Redsparrow77 <jayyzero77@gmail.com>
2022-09-14 15:27:03 +0700

    faet: update

--- src/components/Breadcrumbs/BreadcrumbItems.jsx
+++ src/components/Breadcrumbs/BreadcrumbItems.jsx
@@ -1,18 +1,36 @@
+import customAxios from '@/utils/axios'
 import { Breadcrumb } from 'antd'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link, useLocation } from 'react-router-dom'
 
 const BreadcrumbItems = () => {
+	const [eventName, setEventName] = useState('')
 	const { pathname } = useLocation()
+	const { t } = useTranslation()
 	const pathnames = pathname.split('/').filter((item) => item)
 	const capatilize = (s) => s.charAt(0).toUpperCase() + s.slice(1)
 
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await customAxios(
+				`trainings/${pathnames[pathnames.length - 1]}`
+			)
+			setEventName(response.data.data.name)
+		}
+		fetchData()
+	}, [])
+
 	return pathnames.map((name, index) => {
 		const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
+		const routeName = capatilize(name.split('-').join(' '))
 
 		return (
 			<Breadcrumb.Item key={index}>
 				<Link data-testid='itemsLink' to={`${routeTo}`}>
-					{capatilize(name)}
+					{index === pathnames.length - 1
+						? capatilize(eventName)
+						: capatilize(t(routeName))}
 				</Link>
 			</Breadcrumb.Item>
 		)
--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -1,18 +1,21 @@
 import { Breadcrumb } from 'antd'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import BreadcrumbItems from './BreadcrumbItems'
 
-const BreadCrumb = () => (
-	<>
+const BreadCrumb = () => {
+	const { t } = useTranslation()
+
+	return (
 		<Breadcrumb className='centerAll' separator='>'>
 			<Breadcrumb.Item>
 				<Link data-testid='homeLink' to='/'>
-					Home
+					{t('Home')}
 				</Link>
 			</Breadcrumb.Item>
 			<BreadcrumbItems />
 		</Breadcrumb>
-	</>
-)
+	)
+}
 
 export default BreadCrumb
