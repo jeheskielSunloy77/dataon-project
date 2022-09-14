3b09de22e56ae4e4e9bee927d097f5009e1d1947
Redsparrow77 <jayyzero77@gmail.com>
2022-09-14 15:26:41 +0700

    faet: update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
 import { HeaderSection } from '@/components/index'
+import { getUser } from '@/utils/index'
 import { AllTrainings, MyTraining, SearchFilters } from './sections'
 
-const Home = () => (
-	<main className='pageContainer space-y-2'>
-		<HeaderSection />
-		<SearchFilters />
-		<MyTraining />
-		<AllTrainings />
-	</main>
-)
+const Home = () => {
+	const { role } = getUser()
+
+	return (
+		<main className='pageContainer space-y-2'>
+			<HeaderSection />
+			<SearchFilters />
+			{role === 'user' && <MyTraining />}
+			<AllTrainings />
+		</main>
+	)
+}
 
 export default Home
