ec55108630045fcd694a76c17e8b5517f01a9453
Redsparrow77 <jayyzero77@gmail.com>
2022-09-15 19:58:23 +0700

    feat: unit testing update

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,12 @@ import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './utils/AppContext'
-import { matchMediaConfig } from './utils/testUtils'
+import { matchMediaConfig, setupAdmin } from './utils/testUtils'
+
 matchMediaConfig()
+
 test('renders without crashing', () => {
+	setupAdmin()
 	render(
 		<BrowserRouter>
 			<AppProvider>
