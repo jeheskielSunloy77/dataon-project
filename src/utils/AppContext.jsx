7957b9fbf6b22acbf17b672e9afba07b52f3a5bb
Redsparrow77 <jayyzero77@gmail.com>
2022-09-15 17:30:18 +0700

    feat: centralize all fetching data on AppContext

--- src/utils/AppContext.jsx
+++ src/utils/AppContext.jsx
@@ -1,4 +1,8 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import customAxios from './axios'
+import getUser from './getUser'
+import parsePeriod from './parsePeriod'
+import queryPrams from './queryParams'
 
 export const AppContext = createContext()
 
@@ -9,6 +13,50 @@ export const AppProvider = ({ children }) => {
 		isOnline: '',
 		isComplete: '',
 	})
+	const isSearched = Object.values(searchParams).some((param) => param !== '')
+
+	const [myTrainingData, setMyTrainingData] = useState([])
+	const user = getUser()
+	const myTrainingsUrl = queryPrams('trainings', { userId: user?.userId })
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await customAxios.get(myTrainingsUrl)
+			const myTraining = response.data.data.map((training) => {
+				const period = parsePeriod(training.startDate, training.endDate)
+
+				return {
+					...training,
+					period,
+				}
+			})
+
+			setMyTrainingData(myTraining)
+		}
+		fetchData()
+	}, [])
+
+	const [allTrainingData, setAllTrainingData] = useState([])
+	const [pageLimit, setPageLimit] = useState(10)
+	const [dataLength, setDataLength] = useState(0)
+	const allTrainingsUrl = queryPrams('trainings', { page: 1, limit: pageLimit })
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await customAxios.get(allTrainingsUrl)
+			const allTraining = response.data.data.map((training) => {
+				const period = parsePeriod(training.startDate, training.endDate)
+
+				return {
+					...training,
+					period,
+				}
+			})
+			setAllTrainingData(allTraining)
+			setDataLength(response.data.total)
+		}
+		fetchData()
+	}, [allTrainingsUrl])
 
 	return (
 		<AppContext.Provider
@@ -17,6 +65,12 @@ export const AppProvider = ({ children }) => {
 				setDataView,
 				setSearchParams,
 				searchParams,
+				myTrainingData,
+				setMyTrainingData,
+				allTrainingData,
+				dataLength,
+				setPageLimit,
+				isSearched,
 			}}
 		>
 			{children}
