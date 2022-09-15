7ec029dd006507d7c4582ccb98600db33503832a
Redsparrow77 <jayyzero77@gmail.com>
2022-09-15 19:59:11 +0700

    feat: unit testing update

---
+++ src/pages/Page404/Page404.test.jsx
@@ -0,0 +1,25 @@
+import { fireEvent, render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { it } from 'vitest'
+import Page404 from '.'
+
+describe('Page404', () => {
+	it('should render Page404', () => {
+		const { container } = render(
+			<BrowserRouter>
+				<Page404 />
+			</BrowserRouter>
+		)
+		expect(container).toBeDefined()
+	})
+	it('should change page when clicked the button', () => {
+		const { container } = render(
+			<BrowserRouter>
+				<Page404 />
+			</BrowserRouter>
+		)
+		const button = container.querySelector('a')
+		expect(button).toBeDefined()
+		fireEvent.click(button)
+	})
+})
