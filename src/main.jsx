2331b38cc0f9ff58d6388e8258afdcbe691b3360
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2022-10-07 21:20:34 +0700

    feat: update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { AppProvider } from './utils/AppContext'
-import { i18n } from '@/utils/index'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
